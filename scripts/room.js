e49915e2c4e6f81c57c22aa8c8a0e383e6c471e1
Lily Elbel <lelbel@uncc.edu>
2025-03-15 14:53:05 -0400

    adding decorations

--- scripts/room.js
+++ scripts/room.js
@@ -1,71 +1,124 @@
 import Decoration from './decoration.js';
 
-// class that creates the room the user is decoration and tracks their customizations
+// class that creates the room the user is decorating and tracks their customizations
 export default class Room {
     constructor() {
-        //  list of all owned room objects
+        // list of all owned room objects
         this.ownedDecorations = [];
 
-        //  current room decoration
-        this.mat;
-        this.flowerDisplay;
+        // room decoration
+        this.mat = null;
+        this.flowerDisplay = null;
+        this.largeRug = null;
+        this.counter = null;
     }
 
-    //  add a decoration to the array of owned decorations
-    addOwnedDecoration(decoration) {
-        this.ownedDecorations.push(decoration);
 
-        //set added decoration as current decoration
-        this.setDecoration(decoration);
-    }
-
-    //  get array of owned decorations
-    getOwnedDecorations() {
-        return this.ownedDecorations;
-    }
-
-    //  add or swap out a decoration in the room
-    setDecoration(decoration) {
-        switch(decoration.getType()) {
-            case "Mat":
-                this.mat = decoration;
-                break;
-
-            case "Flower Display":
-                this.flowerDisplay = decoration;
-                break;
-            
-            default:
-                console.log("decoration type not found");
-        }
-    }
-
-    //---------GETTERS AND SETTERS---------
-    //  getter for room mat
+    //---------GETTERS---------
+    // getter for room mat
     getMat() {
-        return this.mat.getSprite();
+        return this.mat;
     }
 
     getFlowerDisplay() {
         return this.flowerDisplay;
     }
+
+    getLargeRug() {
+        return this.largeRug;
+    }
+
+    getCounter() {
+        return this.counter;
+    }
+
+
+    //---------IMAGE VISIBILITY---------
+    showMat() {
+        const matImg = document.createElement('img');
+        matImg.src = `/decorations/${this.getMat().getSprite()}`;
+        matImg.alt = "Mat";
+        matImg.id = "mat";
+        document.getElementById("room").append(matImg);
+    }
+    
+    showFlowerDisplay() {
+        const flowerDisplayImg = document.createElement('img');
+        flowerDisplayImg.src = `/decorations/${this.getFlowerDisplay().getSprite()}`;
+        flowerDisplayImg.alt = "Flower Display";
+        flowerDisplayImg.id = "flower-display";
+        document.getElementById("room").appendChild(flowerDisplayImg);
+    }
+
+    showLargeRug() {
+        const largeRugImg = document.createElement('img');
+        largeRugImg.src = `/decorations/${this.getLargeRug().getSprite()}`;
+        largeRugImg.alt = "Large Rug";
+        largeRugImg.id = "large-rug";
+        document.getElementById("room").appendChild(largeRugImg);
+    }
+
+    showCounter() {
+        const counterImg = document.createElement('img');
+        counterImg.src = `/decorations/${this.getCounter().getSprite()}`;
+        counterImg.alt = "Counter";
+        counterImg.id = "counter";
+        document.getElementById("room").appendChild(counterImg);
+    }
+
+
+    //---------DECORATION METHODS---------
+    // add a decoration to the array of owned decorations
+    addDecoration(decoration) {
+        this.ownedDecorations.push(decoration);
+
+        // set added decoration as current decoration
+        this.setDecoration(decoration);
+    }
+
+    // get array of owned decorations
+    getOwnedDecorations() {
+        return this.ownedDecorations;
+    }
+
+    // add or swap out a decoration in the room
+    setDecoration(decoration) {
+        switch (decoration.getType()) {
+            case "Mat":
+                this.mat = decoration;
+                this.showMat();
+                break;
+
+            case "Flower Display":
+                this.flowerDisplay = decoration;
+                this.showFlowerDisplay();
+                break;
+
+            case "Large Rug":
+                this.largeRug = decoration;
+                this.showLargeRug();
+                break;
+
+            case "Counter":
+                this.counter = decoration;
+                this.showCounter();
+                break;
+
+            default:
+                console.log("Decoration type not found");
+        }
+    }
 }
 
-//TESTING
+// TESTING
 const room = new Room();
-const welcomeMatHello = new Decoration("Welcome Mat Hello", 10, "Mat", "welcomeMatHello.png");
-const welcomeMatBlank = new Decoration("Welcome Mat Blank", 5, "Mat", "welcomeMatBlank.png");
+
+const welcomeMat = new Decoration("Welcome Mat", 10, "Mat", "welcomeMatHello.png");
 const flowerDisplay = new Decoration("Flower Display", 5, "Flower Display", "flowerDisplay.png");
+const largeRug = new Decoration("Large Rug", 5, "Large Rug", "largeRug.png");
+const counter = new Decoration("Counter", 5, "Counter", "counter.png");
 
-room.addOwnedDecoration(welcomeMatHello);
-room.addOwnedDecoration(welcomeMatBlank);
-room.addOwnedDecoration(flowerDisplay);
-
-document.getElementById("test").innerHTML = (room.getOwnedDecorations()[0].getName());
-
-document.getElementById("room").innerHTML = 
-    `
-    <img src="/decorations/background.png" alt="Room Background" id="room-background">
-    <img src="/decorations/${room.getMat()}" id="welcome-mat"/>
-    <img src="/decorations/${room.getFlowerDisplay()}" id="welcome-mat"/>
-    `
+room.addDecoration(welcomeMat);
+room.addDecoration(flowerDisplay);
+room.addDecoration(largeRug);
+room.addDecoration(counter)
